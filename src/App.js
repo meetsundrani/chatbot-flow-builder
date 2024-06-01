d4945b7b76013c9387c5fca5548112a564532e63
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2024-06-01 17:21:45 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import FlowCanvas from './component/FlowCanvas';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      //my component to render
+    </div>
+  );
+}
+
+export default App;
